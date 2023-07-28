a269da5f9cc6524a720978b005ffd49f9a1525a4
ShadowFoxy32 <DeRailedZack@hotmail.com>
2023-07-28 04:29:06 -0400

    Fix naming inconsistency

--- ScriptAppFramework/Script_AppFramework.c
+++ ScriptAppFramework/Script_AppFramework.c
@@ -1,12 +1,12 @@
 #include "Script_AppFramework.h"
 #include <stdlib.h>
-Appframework_Lib* CreateAppFrameworkLib(const char* name, void* Init_Func, void* RegisterApi_Func, void* ScriptError_Func) {
+Appframework_Lib* CreateAppFrameworkLib(const char* name, void* init_func, void* registerapi_func, void* scripterror_func) {
 	Appframework_Lib* appframeworklib = malloc(sizeof(Appframework_Lib));
 	if (appframeworklib != NULL) {
 		appframeworklib->name = name;
-		appframeworklib->Init_Func = Init_Func;
-		appframeworklib->RegisterApi_Func = RegisterApi_Func;
-		appframeworklib->ScriptError_Func = ScriptError_Func;
+		appframeworklib->Init_Func = init_func;
+		appframeworklib->RegisterApi_Func = registerapi_func;
+		appframeworklib->ScriptError_Func = scripterror_func;
 		return appframeworklib;
 	}
 	return NULL;
